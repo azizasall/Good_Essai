30ef78dfc99fde3cb08d33dd73e67de93bc26f49
azizasall <elhadji-abdou-aziz.sall.1@ulaval.ca>
2020-10-21 16:59:24 -0400

    extraction données table  en fichier .csv

--- sauvegarde_code_R_essai/8_oct_essai.R
+++ sauvegarde_code_R_essai/8_oct_essai.R
@@ -1,20 +1,12 @@
 # traitement des données pour construire ma base données
-rm(list=ls())
-
 
 library(readxl)
-X3nd_extraction_financial_data_BLOOMBERG <- read_excel("C:/Users/aziza/Desktop/Session_3/Projet de fin d'études en gestion financière/data_traiement_dans_R/ESSAI_DATA_GIT/3nd_extraction_financial_data_BLOOMBERG.xlsx")
+X3nd_extraction_financial_data_BLOOMBERG <- read_excel("C:/Users/aziza/Desktop/Session_3/Projet de fin d'études en gestion financière/data_traiement_dans_R/GOOD/3nd_extraction_financial_data_BLOOMBERG.xlsx")
 View(X3nd_extraction_financial_data_BLOOMBERG)
 
 
 # traitement des données
 my_data <- data.frame(X3nd_extraction_financial_data_BLOOMBERG)
-View(my_data)
-
-
-#col_name
-col_names <- colnames(my_data[1:19])
-col_names
 
 #extraction de mes tickers
 my_tickers <- my_data$Ticker
@@ -23,13 +15,13 @@ length(my_tickers)
 #extraction de mes ratings
 my_ratings <- my_data$Ratings
 length(my_ratings)
-#View(my_ratings)
+View(my_ratings)
 
 dim(my_data)
 
 #extraction de mes dates
 my_dates <- my_data$Dates
-#View(my_dates)
+View(my_dates)
 length(my_dates)
 
 #data sans tenir compte des tickers et ratings et Dates 
@@ -92,76 +84,5 @@ View(data_2018)
 
 #je peux now rajouter de nouveau les tickers et les ratings pour une meilleur vue de ce que je vais régresser
 
-#mais pour cela comme dans ma data_base y a un ticker sauter je ne vais pas
-# pouvoir le record sur un data frame car not same number of column donc 
-# solution le mettre dabs ue liste et transformer la liste en data frame
-
-
-#*******************************************************
-## a delete juste pour les besoins de éliminer 1e ligne pour avoir 146
-data_2018 <- data_2018[1:nrow(data_2018)-1,]
-dim(data_2018)
-#*******************************************************
-
-#si même nb de ligne plus besoin de le put dans liste
-#data_2018 <- list(my_tickers, my_ratings, data_2018[,])
-
-data_2018 <- data.frame(my_tickers, my_ratings, data_2018[,])
+data_2018 <- data.frame(my_tickers, my_ratings, data_2018)
 View(data_2018)
-# utilisons col_names pour nommer nos colonnes
-
-colnames(data_2018) <- col_names
-
-ncol(data_2018)
-
-names(data_2018)
-
-
-dput(names(data_2018))
-
-View(data_2018)
-#exporter data_2018 sur excel
-
-data.frame(data_2018)
-is.list(data_2018) #ça retourne TRUE
-
-
-sapply(data_2018, class) #pour voir ce qui est liste ou pas
-
-my_extract_data <- data.frame(sapply(data_2018, unlist))  # pour corriger
-
-#write.csv(my_extract_data, file = "data_base_2018") # on fait l'extraction 
-#write.csv(my_extract_data, file = "data_base_2018.csv") # si on oublie le .csv le dossier ne sera pas dans le wd
-
-# write.table() aussi existe
-#getwd  # pour know mon working directory
-
-
-
-# commande pour afficher table de données
-fix(data_2018)
-
-#View(data_2018)
-
-#transformation des ratings en valeurs numériques qu'on  pourra utiliser dans notre 
-# régression
-
-my_ratings
-
-
-
-
-# construction de mes variables finales
-# division ou addition entre varaibles pour avoir good variable
-
-
-
-
-## mettre rating en factor (car need de spécifier leur niveau)
-# idée from vidéo youtube
-
-
-# après on fixe la reférence
-
-
-
